29398263daca887efd1f2e008b5f8f0c93fe9d45
Chunmun <xilasirc@gmail.com>
2013-09-30 19:38:07 +0800

    Add FastClick js

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,3 +1,8 @@
+// Add fastclick to controllers
+window.addEventListener('load', function() {
+    FastClick.attach(document.body);
+}, false);
+
 var controlclick = function() {
 	console.log('clicked');
 	socket.emit('controlclick', { name: myname, click: 1 });
